a399ba20fb2abc7da5038b902548baa5ecbd464e
raulvelasco <uo294946@uniovi.es>
2025-04-13 08:23:50 +0200

    appointments development

--- lib/views/appointments_screen.dart
+++ lib/views/appointments_screen.dart
@@ -3,34 +3,37 @@ import 'package:project/others/theme.dart'; // Import the theme
 import 'package:project/views/profile_screen.dart'; // Import the ProfileScreen
 
 class AppointmentsScreen extends StatefulWidget {
-  const AppointmentsScreen({Key? key}) : super(key: key);
+  const AppointmentsScreen({super.key});
 
   @override
   State<AppointmentsScreen> createState() => _AppointmentsScreenState();
 }
 
-// Enum para representar las pestañas disponibles en la app
-enum HomeTab { home, medicines, profile }
+// Enum to represent the navigation bar options
+enum NavigationTab { home, medicines, profile }
+
+//Enum to represent the accessible screens for making and seeing appointments
+enum AppointmentScreens {makeAppointment, showAppointments}
 
 class _AppointmentsScreenState extends State<AppointmentsScreen>{
   // El estado guardará la pestaña seleccionada directamente usando el enum
-  HomeTab _selectedTab = HomeTab.home;
+  NavigationTab _selectedTab = NavigationTab.home;
 
   ///Method that lets us perform actions, like changing to another screen, when an item is tapped 
   ///by the user
-  void _onItemTapped(int index) {
+  void _onNavigationItemTapped(int index) {
     setState(() {
-      _selectedTab = HomeTab.values[index];
+      _selectedTab = NavigationTab.values[index];
     });
       // Usamos un `switch` para navegar según la pestaña seleccionada
     switch (_selectedTab) {
-      case HomeTab.home:
+      case NavigationTab.home:
         // We are already in home tab
         break;
-      case HomeTab.medicines:
+      case NavigationTab.medicines:
         // Acción para Medicines
         break;
-      case HomeTab.profile:
+      case NavigationTab.profile:
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => const ProfileScreen()),
@@ -39,6 +42,20 @@ class _AppointmentsScreenState extends State<AppointmentsScreen>{
     }
   }
   
+  ///Method that lets us access the "Make an appointment" and "See appointments" screens
+  void _onItemTapped(AppointmentScreens screen) {
+    AppointmentScreens selectedScreen = screen;
+      // Usamos un `switch` para navegar según la pestaña seleccionada
+    switch (selectedScreen) {
+      case AppointmentScreens.makeAppointment:
+        //Nos cambiamos a la pantalla de make appointments
+        break;
+      case AppointmentScreens.showAppointments:
+        //Nos cambiamos a la pantalla de show appointments
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,13 +72,13 @@ class _AppointmentsScreenState extends State<AppointmentsScreen>{
                   key: const ValueKey('make_appointment_button'),
                   icon: Icons.event_available,
                   label: 'Make an appointment',
-                  onTap: () {},
+                  onTap: () => _onItemTapped(AppointmentScreens.makeAppointment),
                 ),
                 _buildCustomButton(
                   key: const ValueKey('see_appointments_button'),
                   icon: Icons.calendar_month,
                   label: 'Show appointents',
-                  onTap: () {},
+                  onTap: () => _onItemTapped(AppointmentScreens.showAppointments),
                 ),
               ],
             ),
@@ -73,8 +90,8 @@ class _AppointmentsScreenState extends State<AppointmentsScreen>{
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.white70,
         // Usamos el índice del enum para seleccionar la pestaña activa
-        currentIndex: HomeTab.values.indexOf(_selectedTab),
-        onTap: _onItemTapped,
+        currentIndex: NavigationTab.values.indexOf(_selectedTab),
+        onTap: _onNavigationItemTapped,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.medical_services), label: 'Medicines'),
