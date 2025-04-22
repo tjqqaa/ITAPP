3c5b9a5acdfbda04521024b10e5c2874b9faafe3
hixo356 <piotrek.bartczak356@gmail.com>
2025-04-22 18:16:27 +0200

    Changed OpenAPI schema URL

--- healtrack_backend/urls.py
+++ healtrack_backend/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
