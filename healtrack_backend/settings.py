7c010fac32c90e6a7c936d0315a1ba43a19be05e
Pakooo0 <mikistasiak02@gmail.com>
2025-04-29 14:50:12 +0200

    Data base admin change

--- healtrack_backend/settings.py
+++ healtrack_backend/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'healtrack_db',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
