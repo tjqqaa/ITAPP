9bab9b8188d29e4e5dd20a2d3a512e1fc0482f04
raulvelasco <uo294946@uniovi.es>
2025-05-02 12:45:20 +0200

    Basic doctor appointments and preinscriptions screens

--- lib/views/doctors/appointments_doctor_screen.dart
+++ lib/views/doctors/appointments_doctor_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project/models/doctor.dart';
 
-
 class AppointmentsDoctorScreen extends StatefulWidget {
   final Doctor doctor;
 
@@ -11,10 +10,26 @@ class AppointmentsDoctorScreen extends StatefulWidget {
   State<AppointmentsDoctorScreen> createState() => _AppointmentsDoctorScreenState();
 }
 
-class _AppointmentsDoctorScreenState extends State<AppointmentsDoctorScreen>{
+class _AppointmentsDoctorScreenState extends State<AppointmentsDoctorScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final appointments = widget.doctor.appointments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Citas de Dr. ${widget.doctor.surname}'),
+      ),
+      body: appointments.isEmpty
+          ? Center(child: Text('No hay citas registradas.'))
+          : ListView.builder(
+              itemCount: appointments.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.calendar_today),
+                  title: Text(appointments[index]),
+                );
+              },
+            ),
+    );
   }
 }
--- lib/views/doctors/doctor_preinscriptions_screen.dart
+++ lib/views/doctors/doctor_preinscriptions_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project/models/doctor.dart';
 
-
 class DoctorPreinscriptionsScreen extends StatefulWidget {
   final Doctor doctor;
 
@@ -11,10 +10,26 @@ class DoctorPreinscriptionsScreen extends StatefulWidget {
   State<DoctorPreinscriptionsScreen> createState() => _DoctorPreinscriptionsScreenState();
 }
 
-class _DoctorPreinscriptionsScreenState extends State<DoctorPreinscriptionsScreen>{
+class _DoctorPreinscriptionsScreenState extends State<DoctorPreinscriptionsScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final patients = widget.doctor.patients;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preinscripciones de Dr. ${widget.doctor.surname}'),
+      ),
+      body: patients.isEmpty
+          ? const Center(child: Text('No hay preinscripciones registradas.'))
+          : ListView.builder(
+              itemCount: patients.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: const Icon(Icons.person_add_alt_1),
+                  title: Text(patients[index]),
+                );
+              },
+            ),
+    );
   }
 }
